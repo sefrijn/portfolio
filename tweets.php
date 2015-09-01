33d784d1421233720c6e9f954156ce0ed99ab243
Sefrijn <sefrijn@gmail.com>
2015-09-01 17:17:36 +0200

    One more bug fix test

--- tweets.php
+++ tweets.php
@@ -1,8 +1,8 @@
 <?php
 	require('../../../wp-blog-header.php');
 	require_once("lib/twitter/autoload.php"); 
-	require_once("lib/twitter/src/TwitterOAuth.php"); 
-	require_once("lib/twitter/src/Util/JsonDecoder.php");
+	// require_once("lib/twitter/src/TwitterOAuth.php"); 
+	// require_once("lib/twitter/src/Util/JsonDecoder.php");
 	require("lib/settings.php");
 	use Abraham\TwitterOAuth\TwitterOAuth;
 	$connection = new TwitterOAuth($twpublic, $twsecret);
